5d9ca5387783724c48a60cb95c3ead5ff4af5eb4
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-08-18 11:40:36 +0530

    Add about page and connect with main page and give style to anchor tag and theme change apply on anchor tag

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 let btn=document.getElementById("btn")
-
+let anchor=document.getElementById("anchor")
 let a=false
 function handleClick(){
     a=!a
     if (a==true){
        document.body.classList.add("dark")
+       document.body.classList.remove("light")
       btn.textContent='light'
     }
     else{
-          document.body.classList.remove("dark")
+      document.body.classList.add("light")
+        document.body.classList.remove("dark")
       btn.textContent='dark'
     }
-
+  if(document.body.classList=="dark"){
+    btn.classList.add("dark")
+    anchor.classList.add("dark")
+  }
+  else{
+    btn.classList.remove("dark")
+    anchor.classList.remove("dark")
+    anchor.classList.add("light")
+  }
   
  
 }
